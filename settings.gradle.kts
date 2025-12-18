d6546dfeba6b8804b9a75f48a38df5a71d902739
vaishnavigorade <vaishnavigorade44@gmail.com>
2025-12-18 20:59:18 +0530

    Initial commit – Android project source code

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "public_transport_complaint_tracker_main_project"
+include(":app")
+ 
